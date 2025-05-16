a0f8017693a16b6ce4248f135c731fef4a85569c
boxy2004 <lily49815@gmail.com>
2025-05-16 05:35:14 -0500

    Subir proyecto listo para Codemagic

---
+++ Boxy_Ready_For_Codemagic/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Boxy',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Bienvenido a Boxy'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  const MyHomePage({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: const Center(child: Text('Hola, soy Boxy 😄')),
+    );
+  }
+}
